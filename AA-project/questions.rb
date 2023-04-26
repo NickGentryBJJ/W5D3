01b79d4c6c4abf1c4cec82ce6a9db13654380e9b
LetsGitFunky <NickHeinBass@gmail.com>
2023-04-26 13:29:28 -0700

    prepping space

---
+++ AA-project/questions.rb
@@ -0,0 +1,8 @@
+class Questions
+
+    def initialize(options)
+        @title = options['title']
+        @body = options['body']
+    end
+    
+end
---
+++ AA-project/users.rb
@@ -0,0 +1,7 @@
+class Users
+
+    def initialize(options)
+        @fname = options['fname']
+        @lname = options['lname']
+    end
+end
