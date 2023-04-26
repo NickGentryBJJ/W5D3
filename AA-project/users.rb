43982156d629cf2618a63d163bd0f3d11f716199
LetsGitFunky <NickHeinBass@gmail.com>
2023-04-26 15:26:10 -0700

    initialized users.rb

--- AA-project/users.rb
+++ AA-project/users.rb
@@ -1,7 +1,23 @@
 class Users
+    attr_accessor :fname, :lname
 
     def initialize(options)
         @fname = options['fname']
         @lname = options['lname']
+    end
+
+    def self.find_by_name(fname, lname)
+        users = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+        SELECT
+            *
+        FROM
+            users
+        WHERE
+            fname = ? AND
+            lname = ?
+    SQL
+    return nil unless id.length > 0
+
+    Users.new(users.first)
     end
 end
